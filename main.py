e1645736ab2989e23e07c6e3eadcfc697eb7e3b6
ignxcy <ignacyy@proton.me>
2023-05-26 15:22:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-from colorama import Fore 
+from colorama import Fore
 import os
 
-orange='\033[0;33m'
-purple='\033[0;35m'
-pink='\033[0;95m'
+orange = '\033[0;33m'
+purple = '\033[0;35m'
+pink = '\033[0;95m'
 
 def do(action):
-    return eval(action)
+    if action == "":
+        return None 
+    else:
+        return eval(action)
 
 print("Welcome to " + Fore.RED + "P" + orange + "y" + Fore.YELLOW + "R" + Fore.GREEN + "e" + Fore.BLUE + "p" + purple + "l" + pink + "!" + Fore.RESET)
 print("Press ^C any time to exit")
@@ -17,10 +20,12 @@ try:
         action = input("> ")
         try:
             result = do(action)
+
             if result is not None:
                 print(result)
         except Exception as e:
             print("An " + Fore.RED + "error " + Fore.RESET + "occurred!")
             print(e)
 except KeyboardInterrupt:
-    exit()
+    print(Fore.RED + "\nB" + orange + "y" + Fore.YELLOW + "e" + Fore.GREEN + "!" + Fore.RESET)
+    pass
